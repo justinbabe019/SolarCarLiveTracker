05077c1625710324c0fe6bf3c85a18af4ba78ac3
University of Michigan Solar Car Team <ncleong@umich.edu>
2023-10-21 17:49:18 +0930

    done bug fix

--- mqtt.py
+++ mqtt.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 f = open('location.txt', 'a')
 
+#reading from mqtt
 import paho.mqtt.client as mqtt
 
+
+#writing
 now=datetime.now()
 f.write( +", "+now.strftime("%b%d %H:%M:%S")+'\n') 
